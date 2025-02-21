83890db6ee5b5e5039ad236edd314b508e6cdcca
Bogdan Kamyshnikov <kamish@Bogdans-MacBook-Air.local>
2025-02-21 11:26:44 +0300

    feat: Add multiplication function to utils

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int a = 124;
     int b = 466;
-    std::cout << "a + b = "  << add(a, b) << std::endl;
-    std::cout << "a * b = " << multiply(a,b) << std::endl;
+    std::cout << "a*b=" << multiply(a,b) << std::endl;
+    std::cout << "a+b="  << add(a, b) << std::endl;
     return 0;
 }
